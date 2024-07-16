cce307d188bffeecd00118a86d8049631b73a0f9
VladimirKurchavov <himrandir@gmail.com>
2024-07-16 17:52:50 +0700

    env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,11 +4,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['himrandir.zapto.org',]
 
 
 # Application definition
